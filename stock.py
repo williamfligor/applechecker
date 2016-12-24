b12279b3baa10c56181458015130dd8168cf6e3a
William Fligor <will@liveify.com>
2016-12-23 20:56:12 -0500

    Update for pushover

--- stock.py
+++ stock.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import httplib
+import urllib
+import os
 import time
 import sys
-import smtplib
 from socket import gaierror
 import requests
 
@@ -13,16 +15,15 @@ SMS = "http://textbelt.com/text"
 DATEFMT = "%m/%d/%Y %H:%M:%S"
 LOADING = ['-', '\\', '|', '/']
 
-def main(model, zipcode, dest, sec=5, login=None, pwd=None):
+def main(model, zipcode, sec=5):
     good_stores = []
-    my_alert = Alert(dest, login, pwd)
-    initmsg = ("[{0}]start tracking {1} in {2}. "
-               "Alert will be sent to {3}").format(time.strftime(DATEFMT),
-                                                   model, zipcode, dest)
+    alert = Alert()
+    initmsg = ("[{0}]start tracking {1} in {2}. ").format(time.strftime(DATEFMT), model, zipcode)
     print initmsg
-    my_alert.send(initmsg)
+
     params = {'parts.0': model,
               'location': zipcode}
+
     sec = int(sec)
     i, cnt = 0, sec
 
@@ -38,31 +39,31 @@ def main(model, zipcode, dest, sec=5, login=None, pwd=None):
         cnt = 0
 
         try:
-            stores = requests.get(URL, params=params) \
-                    .json()['body']['stores'][:8]
-        except (ValueError, KeyError, gaierror):
+            stores = requests.get(URL, params=params).json()
+            stores = stores['body']['stores'][:8]
+        except (ValueError, KeyError, gaierror) as e:
+            print 'error', e
             print "Failed to query Apple Store"
-            time.sleep(int(sec))
+            time.sleep(sec)
             continue
 
         for store in stores:
             sname = store['storeName']
             item = store['partsAvailability'][model]['storePickupProductTitle']
-            if store['partsAvailability'][model]['pickupDisplay'] \
-                        == "available":
+            if store['partsAvailability'][model]['pickupDisplay'] == "available":
                 if sname not in good_stores:
                     good_stores.append(sname)
                     msg = u"Found it! {store} has {item}! {buy}{model}".format(
                         store=sname, item=item, buy=BUY, model=model)
                     print u"{0} {1}".format(time.strftime(DATEFMT), msg)
-                    my_alert.send(msg)
+                    alert.send(msg)
             else:
                 if sname in good_stores:
                     good_stores.remove(sname)
                     msg = u"Oops all {item} in {store} are gone :( ".format(
                         item=item, store=sname)
                     print u"{0} {1}".format(time.strftime(DATEFMT), msg)
-                    my_alert.send(msg)
+                    alert.send(msg)
 
         if good_stores:
             print "[{current}] Avaiable: {stores}".format(
@@ -71,38 +72,15 @@ def main(model, zipcode, dest, sec=5, login=None, pwd=None):
                         if good_stores else "None")
 
 class Alert(object):
-    def __init__(self, dest, login=None, password=None):
-        self.dest = dest
-        if login and password:
-            self.login = login
-            self.password = password
-            self.send = self.send_email
-        else:
-            self.send = self.send_sms
-
-    def send_email(self, msgbody):
-        message = "From: {0}\nTo: {1}\nSubject: {2}\n\n{3}".format(
-            self.login, self.dest, "Apple Stock Alert", msgbody)
-        try:
-            mailer = smtplib.SMTP('smtp.gmail.com:587')
-        except gaierror:
-            print "Couldn't reach Gmail server"
-            return
-        mailer.ehlo()
-        mailer.starttls()
-        mailer.ehlo()
-        mailer.login(self.login, self.password)
-        mailer.sendmail(self.login, self.dest, message)
-        mailer.close()
-
-    def send_sms(self, message):
-        try:
-            response = requests.post(SMS, data={
-                'number': self.dest, 'message': message}).json()
-            if not response['success']:
-                print response['message']
-        except gaierror:
-            print "Couldn't reach TextBelt"
+    def send(self, body):
+        conn = httplib.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json",
+        urllib.urlencode({
+            "token": os.environ['APP_TOKEN'],
+            "user": os.environ['USER_TOKEN'],
+            "message": body,
+        }), { "Content-type": "application/x-www-form-urlencoded" })
+        conn.getresponse()
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
